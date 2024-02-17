c9b34d158479f1d3399bd5f8230e84397c32d3c6
Роман Бандурин <roman.obj@mail.ru>
2024-02-17 23:44:41 +0300

    added timer design

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,41 +1,51 @@
 <script setup>
-// import { useLocalStorage } from '@vueuse/core'
+import "@fontsource/dseg7-modern/700.css"
+import "@fontsource-variable/roboto-flex"
 
-import { ref, computed } from 'vue';
-import { useInterval } from '@vueuse/core'
+import { ref, computed } from "vue"
+import { useInterval } from "@vueuse/core"
 
-const hours = ref('')
+const hours = ref(null)
 const minutes = ref(null)
 const seconds = ref(null)
-const i = ref(0)
 
 const isStarted = ref(false)
 const isRunning = ref(false)
 const timerWork = ref(0)
 
-const nowDateFormat = new Intl.DateTimeFormat('ru-Ru', { timeZone: 'GMT', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false }).format
+const nowDateFormat = new Intl.DateTimeFormat("ru-Ru", {
+  timeZone: "GMT",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  hour12: false,
+}).format
 
 const { counter, reset, pause, resume } = useInterval(1000, {
   controls: true,
   immediate: false,
-  callback: count => timerWork.value > 0 ? timerWork.value -= 1000 : 0
+  callback: () => (timerWork.value > 0 ? (timerWork.value -= 1000) : resetTimer()),
 })
 counter.value = 10
 const clearInputs = () => {
-  hours.value = ''
-  minutes.value = ''
-  seconds.value = ''
+  hours.value = ""
+  minutes.value = ""
+  seconds.value = ""
 }
 const timesOfTimerInput = computed({
   get() {
-    return [hours.value?.toString()?.padStart(2, "0"), minutes.value?.toString()?.padStart(2, "0"), seconds.value?.toString()?.padStart(2, "0")].join(':')
+    return [
+      hours.value?.toString()?.padStart(2, "0"),
+      minutes.value?.toString()?.padStart(2, "0"),
+      seconds.value?.toString()?.padStart(2, "0"),
+    ].join(":")
   },
   set(value) {
-    const [hoursFunc, minutesFunc, secondsFunc] = value.split(':')
+    const [hoursFunc, minutesFunc, secondsFunc] = value.split(":")
     hours.value = hoursFunc
     minutes.value = minutesFunc
     seconds.value = secondsFunc
-  }
+  },
 })
 
 const resumeTimer = () => {
@@ -50,7 +60,15 @@ const pauseTimer = () => {
 
 const startTimer = () => {
   resumeTimer()
-  timerWork.value = Date.UTC(1970, 0, 1, hours.value, minutes.value, seconds.value, 0)
+  timerWork.value = Date.UTC(
+    1970,
+    0,
+    1,
+    hours.value,
+    minutes.value,
+    seconds.value,
+    0
+  )
   isStarted.value = true
   clearInputs()
 }
@@ -61,52 +79,64 @@ const resetTimer = () => {
   timerWork.value = 0
   isStarted.value = false
 }
-
 </script>
 
 <template>
   <main>
-
-    <h1 class="header">VueJs Timer</h1>
-
     <div class="timer__wrapp">
+      <h1 class="header">VueJs Timer</h1>
       <label class="timer__label">
-        <span>Часов</span>
-        <input type="number" :disabled="isStarted" v-model.number="hours">
-      </label>
-      <label class="timer__label">
-        <span>Минут</span>
-        <input type="number" :disabled="isStarted" v-model.number="minutes">
-      </label>
-      <label class="timer__label">
-        <span>Секунд</span>
-        <input type="number" :disabled="isStarted" v-model.number="seconds">
-      </label>
-      <label class="timer__label">
-        <span>Таймер</span>
-        <input type="time" step="1" :disabled="isStarted" v-model="timesOfTimerInput">
+        <input
+          v-if="!isStarted"
+          type="time"
+          v-model="timesOfTimerInput"
+          step="1"
+        />
+        <input
+          v-else
+          type="time"
+          :value="nowDateFormat(timerWork)"
+          inert
+          step="1"
+        />
       </label>
       <div class="timer__result">
-{{ timesOfTimerInput }}
-        <button :disabled="timesOfTimerInput==='00:00:00'" v-if="!isStarted" @click="startTimer">Start</button>
-        <button v-else @click="resetTimer">Reset</button>
+        <button
+          type="button"
+          v-if="!isStarted"
+          :disabled="timesOfTimerInput === '00:00:00'"
+          @click="startTimer"
+        >
+          Start
+        </button>
+        <button v-else type="button" @click="resetTimer">Reset</button>
         <template v-if="isStarted">
-          <button v-if="!isRunning" @click="resumeTimer">Resume</button>
-          <button v-else @click="pauseTimer">Pause</button>
+          <button v-if="!isRunning" type="button" @click="resumeTimer">
+            Resume
+          </button>
+          <button v-else type="button" @click="pauseTimer">Pause</button>
         </template>
       </div>
-    </div>
-
-    <div>
-      {{ nowDateFormat(timerWork) }}
     </div>
   </main>
 </template>
 
 <style scoped>
+main {
+  background-color: #2c2c2c;
+  padding: 5em;
+}
+
+.header {
+  font-family: "Roboto Flex Variable", sans-serif;
+  font-size: 32px;
+  color: #b3b3b3;
+}
+
 .timer__label {
   display: flex;
-  margin-bottom: 20px;
+  margin-bottom: 2em;
+  margin-right: -8.5em;
 }
 
 .timer__wrapp {
@@ -114,5 +144,52 @@ const resetTimer = () => {
   justify-content: center;
   flex-direction: column;
   align-items: center;
+}
+
+.timer__result {
+  display: flex;
+  gap: 2em;
+  font-family: "DSEG7 Modern", sans-serif;
+}
+
+input[type="time"] {
+  font-family: "DSEG7 Modern", sans-serif;
+  font-size: 120px;
+  color: #b3b3b3;
+  border: none;
+  background-color: #2c2c2c;
+}
+
+input[type="time"]::-webkit-datetime-edit-hour-field:focus,
+input[type="time"]::-webkit-datetime-edit-minute-field:focus,
+input[type="time"]::-webkit-datetime-edit-second-field:focus {
+  background-color: grey;
+  color: #2c2c2c;
+}
+
+input[type="time"]::-webkit-calendar-picker-indicator {
+  filter: invert(66%) sepia(0%) saturate(7414%) hue-rotate(30deg)
+    brightness(119%) contrast(69%);
+}
+
+button[type="button"] {
+  cursor: pointer;
+  border: none;
+  padding: 0px 12px;
+  border-radius: 4px;
+  box-shadow: #a1a1a1 0px 6px 0px;
+  font-family: "Roboto Flex Variable", sans-serif;
+  font-size: 22px;
+  height: 55px;
+  color: #2c2c2c;
+  font-weight: bold;
+  width: 200px;
+  background-color: #b3b3b3;
+  transition: color 0.5s ease-in-out 0s;
+}
+
+button[type="button"]:active {
+  transform: translateY(6px);
+  box-shadow: none;
 }
 </style>
